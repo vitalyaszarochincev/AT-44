f81cc6db3f23aac27a4ba5fd050cd86854fd3960
vitalyaszarochincev <pinosol94@gmail.com>
2015-10-30 19:31:09 +0600

    123

--- cip/main.cpp
+++ cip/main.cpp
@@ -1,27 +1,190 @@
-#include "prog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
 
-int main(int argc, char* argv[])
+#define REQUIRED_NUM_OF_PARAMETERS 3
+#define NAME_TAIL 4
+#define ENCODE "-e"
+#define DECODE "-d"
+#define MAX_STR_LEN 256
+#define CONTROL_STR_LENGHT 4
+
+using std::cout;
+using std::endl;
+
+const char controlStr[] = {"lab2"};
+
+enum Errors
 {
-    checkParameters(argc);
+	INCORRECT_PARAMETERS = 1,
+	INCORRECT_FILE = 2
+};
 
-    FILE* inputFile = fopen(argv[1], "r");
-    checkFile(inputFile, argv[1]);
+void __exit(Errors);
+inline void checkParameters(int);
+inline void checkFile(FILE*);
+void createFileName(char*);
+void encode(FILE*, FILE*, char);
+void decode(FILE*, char);
+long countFileLenght(FILE*);
+inline void formatFile(FILE*);
+void checkFormat(FILE*);
+char createKey(char*);
 
-    char* newName = new char[strlen(argv[1])];
-    createName(argv[1], newName);
+int main(int argc, char** argv)
+{
+	checkParameters(argc);
 
-    FILE* outFile = fopen(newName, "wb");
-    checkFile(outFile, newName);
+    if(!strcmp(argv[1], ENCODE))
+    {
+        FILE* txtFile = fopen(argv[2], "r");
+        checkFile(txtFile);
 
-    delete newName;
-    fcloseall();
+        createFileName(argv[2]);
+        FILE* binFile = fopen(argv[2], "wb");
+        checkFile(binFile);
+        formatFile(binFile);
 
-    return 0;
+        memset(&argv[2][strlen(argv[2]) - NAME_TAIL], 0, sizeof(char) * NAME_TAIL);
+        char key = createKey(argv[2]);
+
+        encode(txtFile, binFile, key);
+
+        fclose(txtFile);
+        fclose(binFile);
+    }else if(!strcmp(argv[1], DECODE))
+    {
+        FILE* binFile = fopen(argv[2], "rb");
+        checkFile(binFile);
+        checkFormat(binFile);
+
+        memset(&argv[2][strlen(argv[2]) - NAME_TAIL], 0, sizeof(char) * NAME_TAIL);
+        char key = createKey(argv[2]);
+
+        decode(binFile, key);
+    }else
+    {
+        __exit(INCORRECT_PARAMETERS);
+    }
+
+	return 0;
 }
 
-void createName(char* oldName, char* newName)
+inline void checkParameters(int argc)
 {
-    strcpy(newName, oldName);
-    memset(&newName[strlen(newName) - 3], 0, sizeof(char) * 3); //clean last symbols 3 in name (txt)
-    strcat(newName, "bin");
+	if(argc != REQUIRED_NUM_OF_PARAMETERS)
+		 __exit(INCORRECT_PARAMETERS);
+}
+
+void __exit(Errors errorCode)
+{
+	switch(errorCode)
+	{
+	case INCORRECT_PARAMETERS:
+		cout << "incorrect parameters" << endl;
+		break;
+
+	case INCORRECT_FILE:
+		cout << "incorrect file" << endl;
+		break;
+	}
+
+	exit(0);
+}
+
+inline void checkFile(FILE* file)
+{
+	if(!file)
+		__exit(INCORRECT_FILE);
+}
+
+void createFileName(char* oldName)
+{
+	size_t oldNameLenght = strlen(oldName);
+
+	strcpy(&oldName[oldNameLenght - NAME_TAIL], ".bin");
+}
+
+void encode(FILE* txtFile, FILE* binFile, char key)
+{
+    char str[MAX_STR_LEN] = {0};
+    char strLenght = 0;
+    char encodedStrLenght = 0;
+
+    while(fgets(str, MAX_STR_LEN, txtFile))
+    {
+        strLenght = (char)strlen(str) - 1;
+
+        for(char i = 0; i < strLenght; i++)
+        {
+            str[i] ^= key;
+        }
+
+        encodedStrLenght = strLenght ^ key;
+
+        fwrite(&encodedStrLenght, sizeof(char), 1, binFile);
+        fwrite(str, sizeof(char), strLenght, binFile);
+    }
+}
+
+void decode(FILE* file, char key)
+{
+    char str[MAX_STR_LEN] = {0};
+    char strLenght = 0;
+    size_t i = (size_t)countFileLenght(file) - CONTROL_STR_LENGHT;
+
+    fseek(file, CONTROL_STR_LENGHT, SEEK_SET);
+
+    while(i)
+    {
+        i -= fread(&strLenght, sizeof(char), 1, file);
+
+        strLenght ^= key;
+        i -= fread(str, sizeof(char), strLenght, file);
+
+        for(char i = 0; i < strLenght; i++)
+        {
+            str[i] ^= key;
+        }
+
+        printf("%d %s\n", strLenght, str);
+        memset(str, 0, sizeof(char) * MAX_STR_LEN);
+    }
+}
+
+inline void formatFile(FILE* file)
+{
+    fwrite(controlStr, sizeof(char), CONTROL_STR_LENGHT, file);
+}
+
+long countFileLenght(FILE* file)
+{
+    fseek(file, 0, SEEK_END);
+    long result = ftell(file);
+    rewind(file);
+
+    return result;
+}
+
+void checkFormat(FILE* file)
+{
+    char contStr[CONTROL_STR_LENGHT] = {0};
+
+    fread(contStr, sizeof(char), CONTROL_STR_LENGHT, file);
+
+    if(strcmp(controlStr, contStr))
+        __exit(INCORRECT_FILE);
+}
+
+char createKey(char* name)
+{
+    long long key = 0;
+
+    while(*name)
+    {
+        key += key * 255 + *name;
+        name++;
+    }
+    return (char)(key % 256);
 }
